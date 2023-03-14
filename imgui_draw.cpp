markvilar/imgui

Texture-based round corners: Adding style/drawlist flags for rounded corner textures

imgui_draw.cpp
@@ -1026,8 +1026,14 @@ void ImDrawList::AddLine(const ImVec2& p1, const ImVec2& p2, ImU32 col, float th
 // We are using the textures generated by ImFontAtlasBuildRenderRoundCornersTexData()
 inline bool AddRoundCornerRect(ImDrawList* draw_list, const ImVec2& a, const ImVec2& b, ImU32 col, float rounding, ImDrawCornerFlags rounding_corners_flags, bool fill)
 {
+    if (!(draw_list->Flags & ImDrawListFlags_TexturedRoundCorners)) // Disabled by the draw list flags
+        return false;
+
     const ImDrawListSharedData* data = draw_list->_Data;
     const int rad = (int)rounding;
+
+    if (data->Font->ContainerAtlas->Flags & ImFontAtlasFlags_NoTexturedRoundCorners) // No data in font
+        return false;
 
     if ((rad <= 0) || // Zero radius causes issues with the [rad - 1] UV lookup below
         (rad > data->Font->ContainerAtlas->RoundCornersMaxSize))
@@ -1038,9 +1044,7 @@ inline bool AddRoundCornerRect(ImDrawList* draw_list, const ImVec2& a, const ImV
 
     // Debug command to force this render path to only execute when shift is held
     if (!ImGui::GetIO().KeyShift)
-    {
         return false;
-    }
 
     ImTextureID tex_id = data->Font->ContainerAtlas->TexID;
     IM_ASSERT(tex_id == draw_list->_TextureIdStack.back());  // Use high-level ImGui::PushFont() or low-level ImDrawList::PushTextureId() to change font.
@@ -1391,9 +1395,15 @@ void ImDrawList::AddTriangleFilled(const ImVec2& p1, const ImVec2& p2, const ImV
 // (in which case the caller should try the regular circle drawing code)
 inline bool AddRoundCornerCircle(ImDrawList* draw_list, const ImVec2& center, float radius, ImU32 col, bool fill)
 {
+    if (!(draw_list->Flags & ImDrawListFlags_TexturedRoundCorners)) // Disabled by the draw list flags
+        return false;
+
     const ImDrawListSharedData* data = draw_list->_Data;
     ImTextureID tex_id = data->Font->ContainerAtlas->TexID;
     IM_ASSERT(tex_id == draw_list->_TextureIdStack.back());  // Use high-level ImGui::PushFont() or low-level ImDrawList::PushTextureId() to change font.
+
+    if (data->Font->ContainerAtlas->Flags & ImFontAtlasFlags_NoTexturedRoundCorners) // No data in font
+        return false;
 
     const int rad = (int)radius;
     if (rad < 1 || rad > data->Font->ContainerAtlas->RoundCornersMaxSize) // Radius 0 will cause issues with the UV lookup below
@@ -2043,7 +2053,7 @@ ImFontAtlas::ImFontAtlas()
     TexUvScale = ImVec2(0.0f, 0.0f);
     TexUvWhitePixel = ImVec2(0.0f, 0.0f);
 
-    RoundCornersMaxSize = 60;
+    RoundCornersMaxSize = 32;
     for (int n = 0; n < IM_ARRAYSIZE(CustomRectIds); n++)
         CustomRectIds[n] = -1;
 }

jfrase8/ChessBros

imgui_draw.cpp
@@ -1389,6 +1389,9 @@ void ImDrawList::AddLine(const ImVec2& p1, const ImVec2& p2, ImU32 col, float th
 // We are using the textures generated by ImFontAtlasBuildRenderRoundCornersTexData()
 inline bool AddRoundCornerRect(ImDrawList* draw_list, const ImVec2& a, const ImVec2& b, ImU32 col, float rounding, ImDrawFlags flags, bool fill)
 {
+    if (!(draw_list->Flags & ImDrawListFlags_TexturedRoundCorners)) // Disabled by the draw list flags
+        return false;
+
 #if 1
     flags = FixRectCornerFlags(flags);
     rounding = ImMin(rounding, ImFabs(b.x - a.x) * (((flags & ImDrawFlags_RoundCornersTop) == ImDrawFlags_RoundCornersTop) || ((flags & ImDrawFlags_RoundCornersBottom) == ImDrawFlags_RoundCornersBottom) ? 0.5f : 1.0f) - 1.0f);
@@ -1397,6 +1400,9 @@ inline bool AddRoundCornerRect(ImDrawList* draw_list, const ImVec2& a, const ImV
 
     const ImDrawListSharedData* data = draw_list->_Data;
     const int rad = (int)rounding;
+
+    if (data->Font->ContainerAtlas->Flags & ImFontAtlasFlags_NoBakedRoundCorners) // No data in font
+        return false;
 
     if ((rad <= 0) || // Zero radius causes issues with the [rad - 1] UV lookup below
         (rad > data->Font->ContainerAtlas->RoundCornersMaxSize))
@@ -1407,9 +1413,7 @@ inline bool AddRoundCornerRect(ImDrawList* draw_list, const ImVec2& a, const ImV
 
     // Debug command to force this render path to only execute when shift is held
     if (!ImGui::GetIO().KeyShift)
-    {
         return false;
-    }
 
     ImTextureID tex_id = data->Font->ContainerAtlas->TexID;
     IM_ASSERT(tex_id == draw_list->_TextureIdStack.back());  // Use high-level ImGui::PushFont() or low-level ImDrawList::PushTextureId() to change font.
@@ -1766,9 +1770,15 @@ void ImDrawList::AddTriangleFilled(const ImVec2& p1, const ImVec2& p2, const ImV
 // (in which case the caller should try the regular circle drawing code)
 inline bool AddRoundCornerCircle(ImDrawList* draw_list, const ImVec2& center, float radius, ImU32 col, bool fill)
 {
+    if (!(draw_list->Flags & ImDrawListFlags_TexturedRoundCorners)) // Disabled by the draw list flags
+        return false;
+
     const ImDrawListSharedData* data = draw_list->_Data;
     ImTextureID tex_id = data->Font->ContainerAtlas->TexID;
     IM_ASSERT(tex_id == draw_list->_TextureIdStack.back());  // Use high-level ImGui::PushFont() or low-level ImDrawList::PushTextureId() to change font.
+
+    if (data->Font->ContainerAtlas->Flags & ImFontAtlasFlags_NoBakedRoundCorners) // No data in font
+        return false;
 
     const int rad = (int)radius;
     if (rad < 1 || rad > data->Font->ContainerAtlas->RoundCornersMaxSize) // Radius 0 will cause issues with the UV lookup below
@@ -2425,7 +2435,7 @@ ImFontAtlas::ImFontAtlas()
     TexGlyphPadding = 1;
     PackIdMouseCursors = PackIdLines = -1;
 
-    RoundCornersMaxSize = 60;
+    RoundCornersMaxSize = 32;
 }
 
 ImFontAtlas::~ImFontAtlas()